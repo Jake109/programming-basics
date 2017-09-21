39e8f5661c2b6296de803e1802721f1283437789
Jake109 <jake.kokkonen@gmail.com>
2017-09-21 15:47:00 +0300

    almost done

---
+++ conditional-statements/conditional-statements/conditional-statements/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace conditional_statements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma tulostaa positiivinen, negatiivinen tai nolla");
+            Console.Write("syötä luku: ");
+            string userInput;
+            userInput = Console.ReadLine();
+
+
+            Console.WriteLine("Syötit arvon (0)",userInput);
+            Console.WriteLine($"Syötit arvon (userInput)");
+            Console.ReadKey();
+        }
+    }
+}
