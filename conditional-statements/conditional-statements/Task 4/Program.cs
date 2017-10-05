cb7476ae7cf36aa5aa9bf888adec9307301f4205
Jake109 <jake.kokkonen@gmail.com>
2017-10-05 08:12:49 +0300

    Task 4

---
+++ conditional-statements/conditional-statements/Task 4/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Syötä numero1");
+            string numero1 = Console.ReadLine();
+            int num1 = int.Parse(numero1);
+            Console.WriteLine(num1);
+
+            Console.WriteLine("Syötä numero2");
+            string numero2 = Console.ReadLine();
+            int num2 = int.Parse(numero2);
+            Console.WriteLine(num2);
+
+            Console.WriteLine("Syötä numero3");
+            string numero3 = Console.ReadLine();
+            int num3 = int.Parse(numero3);
+            Console.WriteLine(num3);
+
+            
+
+            if (num1 < num2)
+            {
+                if (num1 < num3)
+                {
+                    if (num2 < num3)
+                    {
+                        Console.WriteLine($"{num1}, {num2}, {num3}");
+                    }
+                    else
+                        Console.WriteLine($"{num1}, {num3}, {num2}");
+                }
+                else
+                    Console.WriteLine($"{num3}, {num1}, {num2}");
+            }
+            else
+                if (num1 < num3)
+                {
+                    Console.WriteLine($"{num2}, {num1}, {num3}");
+                }
+                else
+                    if (num2 < num3)
+                    {
+                        Console.WriteLine($"{num2}, {num3}, {num1}");
+                    }
+                    else
+                        Console.WriteLine($"{num3}, {num2}, {num1}");     
+
+
+
+
+                Console.ReadKey();
+        }
+    }
+}
---
+++ conditional-statements/conditional-statements/Task 5/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int price = 16;
+            int discount = 0;
+            bool discounted = false;
+
+            Console.WriteLine("Anna ikasi");
+            string age1 = Console.ReadLine();
+            int age = int.Parse(age1);
+
+            if (age <= 7)
+            {
+                discounted = true;
+                price = 0;
+                Console.WriteLine("Alle 7 vuotiaat paasevat ilmaiseksi");
+
+            }
+        
+                
+
+
+            Console.WriteLine(age);
+
+
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
